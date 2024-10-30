5e2769d00dbb56b2dd7beaf03950a11d2ab542f7
James Crittenden <Crittenden.work@gmail.com>
2024-10-30 12:37:41 -0400

    Kickstarting the project by pseudocoding the Terraform environment.

---
+++ main.tf
@@ -0,0 +1,11 @@
+# Set up GCP provider authentication
+# Configure project, region, and credentials
+
+# Define GKE (Google Kubernetes Engine) cluster resources
+# Specify node pool settings, such as machine type, number of nodes
+
+# Set up networking resources
+# Define VPC, subnets, firewall rules if necessary
+
+# Output essential information
+# Cluster endpoint, kubeconfig, etc.
