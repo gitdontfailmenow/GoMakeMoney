8506d0684121e3ecd8dcf96e4240e0797e32bf91
James Crittenden <Crittenden.work@gmail.com>
2024-10-30 12:46:04 -0400

    Add the GCP Block to the main.tf file

--- main.tf
+++ main.tf
@@ -1,5 +1,11 @@
 # Set up GCP provider authentication
 # Configure project, region, and credentials
+provider "google" {
+  # Specify project ID
+  # Set the region for resources
+  # Define authentication method (e.g., using credentials file)
+}
+
 
 # Define GKE (Google Kubernetes Engine) cluster resources
 # Specify node pool settings, such as machine type, number of nodes
